e1f0b422432288d674185ab711e020f7c4f9a4a6
juniaskoudou <130841755+juniaskoudou@users.noreply.github.com>
2025-03-19 17:08:15 +0100

    fixed og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: 'Junias K. - Product Designer & Photographer',
+        type: 'image/jpeg',
       }
     ],
   },
